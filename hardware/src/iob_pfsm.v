e327e2ab4351a405dddbf11459b2e6877b348de8
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-01-22 03:45:23 +0000

    chore(swreg): Update swreg connections according to new iob-soc

--- hardware/src/iob_pfsm.v
+++ hardware/src/iob_pfsm.v
@@ -11,20 +11,6 @@ module iob_pfsm # (
    `include "iob_pfsm_io.vs"
   );
 
-   `include "iob_wire.vs"
-
-   assign iob_valid = iob_valid_i;
-   assign iob_addr = iob_addr_i;
-   assign iob_wdata = iob_wdata_i;
-   assign iob_wstrb = iob_wstrb_i;
-   assign iob_rvalid_o = iob_rvalid;
-   assign iob_rdata_o = iob_rdata;
-   assign iob_ready_o = iob_ready;
-
-   //Dummy iob_ready_nxt_o and iob_rvalid_nxt_o to be used in swreg (unused ports)
-   wire iob_ready_nxt;
-   wire iob_rvalid_nxt;
-
   //BLOCK Register File & Configuration control and status register file.
   `include "iob_pfsm_swreg_inst.vs"
 
