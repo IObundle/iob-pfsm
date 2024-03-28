030af5e85fb333207a6d7fff7b3c325b0d6c69ba
PMiranda <pmiranda.iobundle@gmail.com>
2024-03-28 18:25:47 +0000

    feat(drivers): multiple hardware device support - update iob-pfsm drivers to support multiple hardware devices     - change number of supported devices with `NUM_DEVICES` in       `iob_pfsm_main.c`

--- software/linux/drivers/iob_pfsm_main.c
+++ software/linux/drivers/iob_pfsm_main.c
@@ -11,26 +11,39 @@
 #include <linux/kernel.h>
 #include <linux/mod_devicetable.h>
 #include <linux/module.h>
+#include <linux/mutex.h>
 #include <linux/platform_device.h>
 #include <linux/uaccess.h>
 
 #include "iob_class/iob_class_utils.h"
 #include "iob_pfsm.h"
 
+#define NUM_DEVICES 2
+
 static int iob_pfsm_probe(struct platform_device *);
 static int iob_pfsm_remove(struct platform_device *);
 
 static ssize_t iob_pfsm_read(struct file *, char __user *, size_t, loff_t *);
 static ssize_t iob_pfsm_write(struct file *, const char __user *, size_t,
-                               loff_t *);
+                              loff_t *);
 static loff_t iob_pfsm_llseek(struct file *, loff_t, int);
 static int iob_pfsm_open(struct inode *, struct file *);
 static int iob_pfsm_release(struct inode *, struct file *);
 
-static struct iob_data iob_pfsm_data = {0};
 DEFINE_MUTEX(iob_pfsm_mutex);
+struct pfsm_driver {
+  dev_t devnum;
+  struct class *class;
+  struct list_head list;
+};
 
-#include "iob_pfsm_sysfs.h"
+static struct pfsm_driver pfsm_driver = {
+    .devnum = 0,
+    .class = NULL,
+    .list = LIST_HEAD_INIT(pfsm_driver.list),
+};
+
+#include "iob_pfsm_sysfs_multi.h"
 
 static const struct file_operations iob_pfsm_fops = {
     .owner = THIS_MODULE,
@@ -63,14 +76,28 @@ static struct platform_driver iob_pfsm_driver = {
 static int iob_pfsm_probe(struct platform_device *pdev) {
   struct resource *res;
   int result = 0;
+  struct iob_data *iob_pfsm_data = NULL;
 
-  if (iob_pfsm_data.device != NULL) {
+  mutex_lock(&iob_pfsm_mutex);
+  if (MINOR(pfsm_driver.devnum) >= NUM_DEVICES) {
     pr_err("[Driver] %s: No more devices allowed!\n", IOB_PFSM_DRIVER_NAME);
-
     return -ENODEV;
   }
+  mutex_unlock(&iob_pfsm_mutex);
 
   pr_info("[Driver] %s: probing.\n", IOB_PFSM_DRIVER_NAME);
+
+  iob_pfsm_data = (struct iob_data *)devm_kzalloc(
+      &pdev->dev, sizeof(struct iob_data), GFP_KERNEL);
+  if (iob_pfsm_data == NULL) {
+    pr_err("[Driver]: Failed to allocate memory for iob_pfsm_data\n");
+    return -ENOMEM;
+  }
+
+  // add device to list
+  mutex_lock(&iob_pfsm_mutex);
+  list_add_tail(&iob_pfsm_data->list, &pfsm_driver.list);
+  mutex_unlock(&iob_pfsm_mutex);
 
   // Get the I/O region base address
   res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
@@ -81,64 +108,56 @@ static int iob_pfsm_probe(struct platform_device *pdev) {
   }
 
   // Request and map the I/O region
-  iob_pfsm_data.regbase = devm_ioremap_resource(&pdev->dev, res);
-  if (IS_ERR(iob_pfsm_data.regbase)) {
-    result = PTR_ERR(iob_pfsm_data.regbase);
+  iob_pfsm_data->regbase = devm_ioremap_resource(&pdev->dev, res);
+  if (IS_ERR(iob_pfsm_data->regbase)) {
+    result = PTR_ERR(iob_pfsm_data->regbase);
     goto r_ioremmap;
   }
-  iob_pfsm_data.regsize = resource_size(res);
+  iob_pfsm_data->regsize = resource_size(res);
 
-  // Alocate char device
-  result =
-      alloc_chrdev_region(&iob_pfsm_data.devnum, 0, 1, IOB_PFSM_DRIVER_NAME);
+  cdev_init(&iob_pfsm_data->cdev, &iob_pfsm_fops);
+  iob_pfsm_data->cdev.owner = THIS_MODULE;
+  iob_pfsm_data->class = NULL;
+
+  mutex_lock(&iob_pfsm_mutex);
+  iob_pfsm_data->devnum = pfsm_driver.devnum;
+  pfsm_driver.devnum =
+      MKDEV(MAJOR(pfsm_driver.devnum), MINOR(pfsm_driver.devnum) + 1);
+  mutex_unlock(&iob_pfsm_mutex);
+
+  result = cdev_add(&iob_pfsm_data->cdev, iob_pfsm_data->devnum, 1);
   if (result) {
-    pr_err("%s: Failed to allocate device number!\n", IOB_PFSM_DRIVER_NAME);
-    goto r_alloc_region;
-  }
-
-  cdev_init(&iob_pfsm_data.cdev, &iob_pfsm_fops);
-
-  result = cdev_add(&iob_pfsm_data.cdev, iob_pfsm_data.devnum, 1);
-  if (result) {
-    pr_err("%s: Char device registration failed!\n", IOB_PFSM_DRIVER_NAME);
+    pr_err("[Driver] %s: Char device registration failed!\n",
+           IOB_PFSM_DRIVER_NAME);
     goto r_cdev_add;
   }
 
-  // Create device class // todo: make a dummy driver just to create and own the
-  // class: https://stackoverflow.com/a/16365027/8228163
-  if ((iob_pfsm_data.class =
-           class_create(THIS_MODULE, IOB_PFSM_DRIVER_CLASS)) == NULL) {
-    printk("Device class can not be created!\n");
-    goto r_class;
-  }
-
-  // Create device file
-  iob_pfsm_data.device =
-      device_create(iob_pfsm_data.class, NULL, iob_pfsm_data.devnum, NULL,
-                    IOB_PFSM_DRIVER_NAME);
-  if (iob_pfsm_data.device == NULL) {
-    printk("Can not create device file!\n");
+  iob_pfsm_data->device = device_create(
+      pfsm_driver.class, NULL, iob_pfsm_data->devnum, iob_pfsm_data, "%s%d",
+      IOB_PFSM_DRIVER_NAME, MINOR(iob_pfsm_data->devnum));
+  if (iob_pfsm_data->device == NULL) {
+    pr_err("[Driver] %s: Can not create device file!\n", IOB_PFSM_DRIVER_NAME);
     goto r_device;
   }
 
-  result = iob_pfsm_create_device_attr_files(iob_pfsm_data.device);
+  // Associate iob_data to device
+  pdev->dev.platform_data = iob_pfsm_data;              // pdev functions
+  iob_pfsm_data->device->platform_data = iob_pfsm_data; // sysfs functions
+
+  result = iob_pfsm_create_device_attr_files(iob_pfsm_data->device);
   if (result) {
     pr_err("Cannot create device attribute file......\n");
     goto r_dev_file;
   }
 
-  dev_info(&pdev->dev, "initialized.\n");
+  dev_info(&pdev->dev, "initialized with %d\n", MINOR(iob_pfsm_data->devnum));
   goto r_ok;
 
 r_dev_file:
-  iob_pfsm_remove_device_attr_files(&iob_pfsm_data);
+  device_destroy(iob_pfsm_data->class, iob_pfsm_data->devnum);
+  cdev_del(&(iob_pfsm_data->cdev));
 r_device:
-  class_destroy(iob_pfsm_data.class);
-r_class:
-  cdev_del(&iob_pfsm_data.cdev);
 r_cdev_add:
-  unregister_chrdev_region(iob_pfsm_data.devnum, 1);
-r_alloc_region:
   // iounmap is managed by devm
 r_ioremmap:
 r_get_resource:
@@ -148,26 +167,67 @@ r_ok:
 }
 
 static int iob_pfsm_remove(struct platform_device *pdev) {
-  iob_pfsm_remove_device_attr_files(&iob_pfsm_data);
-  class_destroy(iob_pfsm_data.class);
-  cdev_del(&iob_pfsm_data.cdev);
-  unregister_chrdev_region(iob_pfsm_data.devnum, 1);
-  // Note: no need for iounmap, since we are using devm_ioremap_resource()
+  struct iob_data *iob_pfsm_data = (struct iob_data *)pdev->dev.platform_data;
+  iob_pfsm_remove_device_attr_files(iob_pfsm_data);
+  cdev_del(&(iob_pfsm_data->cdev));
 
-  dev_info(&pdev->dev, "exiting.\n");
+  // remove from list
+  mutex_lock(&iob_pfsm_mutex);
+  list_del(&iob_pfsm_data->list);
+  mutex_unlock(&iob_pfsm_mutex);
+
+  // Note: no need for iounmap, since we are using devm_ioremap_resource()
+  dev_info(&pdev->dev, "remove.\n");
 
   return 0;
 }
 
 static int __init iob_pfsm_init(void) {
+  int ret = 0;
   pr_info("[Driver] %s: initializing.\n", IOB_PFSM_DRIVER_NAME);
 
-  return platform_driver_register(&iob_pfsm_driver);
+  // Allocate char device
+  ret = alloc_chrdev_region(&pfsm_driver.devnum, 0, NUM_DEVICES,
+                            IOB_PFSM_DRIVER_NAME);
+  if (ret) {
+    pr_err("[Driver] %s: Failed to allocate char device region\n",
+           IOB_PFSM_DRIVER_NAME);
+    goto r_exit;
+  }
+
+  // Create device class // todo: make a dummy driver just to create and own the
+  // class: https://stackoverflow.com/a/16365027/8228163
+  if ((pfsm_driver.class =
+           class_create(THIS_MODULE, IOB_PFSM_DRIVER_CLASS)) == NULL) {
+    printk("Device class can not be created!\n");
+    goto r_alloc_region;
+  }
+
+  ret = platform_driver_register(&iob_pfsm_driver);
+  if (ret < 0) {
+    pr_err("[Driver] %s: Failed to register platform driver\n",
+           IOB_PFSM_DRIVER_NAME);
+    goto r_class;
+  }
+
+r_class:
+  class_destroy(pfsm_driver.class);
+r_alloc_region:
+  unregister_chrdev_region(pfsm_driver.devnum, NUM_DEVICES);
+r_exit:
+  return ret;
 }
 
 static void __exit iob_pfsm_exit(void) {
   pr_info("[Driver] %s: exiting.\n", IOB_PFSM_DRIVER_NAME);
+
+  mutex_lock(&iob_pfsm_mutex);
+  pfsm_driver.devnum = MKDEV(MAJOR(pfsm_driver.devnum), 0);
+  mutex_unlock(&iob_pfsm_mutex);
+
   platform_driver_unregister(&iob_pfsm_driver);
+
+  unregister_chrdev_region(pfsm_driver.devnum, NUM_DEVICES);
 }
 
 //
@@ -175,12 +235,22 @@ static void __exit iob_pfsm_exit(void) {
 //
 
 static int iob_pfsm_open(struct inode *inode, struct file *file) {
+  struct iob_data *iob_pfsm_data = NULL;
+
   pr_info("[Driver] iob_pfsm device opened\n");
 
   if (!mutex_trylock(&iob_pfsm_mutex)) {
     pr_info("Another process is accessing the device\n");
 
     return -EBUSY;
+  }
+
+  // assign iob_pfsm_data to file private_data
+  list_for_each_entry(iob_pfsm_data, &pfsm_driver.list, list) {
+    if (iob_pfsm_data->devnum == inode->i_rdev) {
+      file->private_data = iob_pfsm_data;
+      return 0;
+    }
   }
 
   return 0;
@@ -195,20 +265,22 @@ static int iob_pfsm_release(struct inode *inode, struct file *file) {
 }
 
 static ssize_t iob_pfsm_read(struct file *file, char __user *buf, size_t count,
-                              loff_t *ppos) {
+                             loff_t *ppos) {
   int size = 0;
   u32 value = 0;
+  struct iob_data *iob_pfsm_data = (struct iob_data *)file->private_data;
 
   /* read value from register */
   switch (*ppos) {
   case IOB_PFSM_CURRENT_STATE_ADDR:
-    value = iob_data_read_reg(iob_pfsm_data.regbase, IOB_PFSM_CURRENT_STATE_ADDR,
-                              IOB_PFSM_CURRENT_STATE_W);
+    value =
+        iob_data_read_reg(iob_pfsm_data->regbase, IOB_PFSM_CURRENT_STATE_ADDR,
+                          IOB_PFSM_CURRENT_STATE_W);
     size = (IOB_PFSM_CURRENT_STATE_W >> 3); // bit to bytes
     pr_info("[Driver] Read CURRENT_STATE!\n");
     break;
   case IOB_PFSM_VERSION_ADDR:
-    value = iob_data_read_reg(iob_pfsm_data.regbase, IOB_PFSM_VERSION_ADDR,
+    value = iob_data_read_reg(iob_pfsm_data->regbase, IOB_PFSM_VERSION_ADDR,
                               IOB_PFSM_VERSION_W);
     size = (IOB_PFSM_VERSION_W >> 3); // bit to bytes
     pr_info("[Driver] Read version!\n");
@@ -229,16 +301,18 @@ static ssize_t iob_pfsm_read(struct file *file, char __user *buf, size_t count,
 }
 
 static ssize_t iob_pfsm_write(struct file *file, const char __user *buf,
-                               size_t count, loff_t *ppos) {
+                              size_t count, loff_t *ppos) {
   int size = 0;
   u32 value = 0;
+  struct iob_data *iob_pfsm_data = (struct iob_data *)file->private_data;
 
   switch (*ppos) {
   case IOB_PFSM_MEM_WORD_SELECT_ADDR:
     size = (IOB_PFSM_MEM_WORD_SELECT_W >> 3); // bit to bytes
     if (read_user_data(buf, size, &value))
       return -EFAULT;
-    iob_data_write_reg(iob_pfsm_data.regbase, value, IOB_PFSM_MEM_WORD_SELECT_ADDR,
+    iob_data_write_reg(iob_pfsm_data->regbase, value,
+                       IOB_PFSM_MEM_WORD_SELECT_ADDR,
                        IOB_PFSM_MEM_WORD_SELECT_W);
     pr_info("[Driver] MEM_WORD_SELECT iob_pfsm: 0x%x\n", value);
     break;
@@ -246,24 +320,26 @@ static ssize_t iob_pfsm_write(struct file *file, const char __user *buf,
     size = (IOB_PFSM_SOFTRESET_W >> 3); // bit to bytes
     if (read_user_data(buf, size, &value))
       return -EFAULT;
-    iob_data_write_reg(iob_pfsm_data.regbase, value, IOB_PFSM_SOFTRESET_ADDR,
+    iob_data_write_reg(iob_pfsm_data->regbase, value, IOB_PFSM_SOFTRESET_ADDR,
                        IOB_PFSM_SOFTRESET_W);
     pr_info("[Driver] SOFTRESET iob_pfsm: 0x%x\n", value);
     break;
   default:
     // MEMORY address range
-    if (((*ppos) >= IOB_PFSM_MEMORY_ADDR) && 
-            ((*ppos) < IOB_PFSM_MEM_WORD_SELECT_ADDR)) {
-        size = (IOB_PFSM_MEMORY_W >> 3); // bit to bytes
-        if (read_user_data(buf, size, &value))
-          return -EFAULT;
-        iob_data_write_reg(iob_pfsm_data.regbase, value, (*ppos),
-                           IOB_PFSM_MEMORY_W);
-        pr_info("[Driver] MEMORY[%x] iob_pfsm: 0x%x\n", value, (unsigned int)((*ppos)-IOB_PFSM_MEMORY_ADDR));
+    if (((*ppos) >= IOB_PFSM_MEMORY_ADDR) &&
+        ((*ppos) < IOB_PFSM_MEM_WORD_SELECT_ADDR)) {
+      size = (IOB_PFSM_MEMORY_W >> 3); // bit to bytes
+      if (read_user_data(buf, size, &value))
+        return -EFAULT;
+      iob_data_write_reg(iob_pfsm_data->regbase, value, (*ppos),
+                         IOB_PFSM_MEMORY_W);
+      pr_info("[Driver] MEMORY[%x] iob_pfsm: 0x%x\n", value,
+              (unsigned int)((*ppos) - IOB_PFSM_MEMORY_ADDR));
+    } else {
+      pr_info("[Driver] Invalid write address 0x%x\n", (unsigned int)*ppos);
+      return 0;
     }
-    pr_info("[Driver] Invalid write address 0x%x\n", (unsigned int)*ppos);
     // invalid address - no bytes written
-    return 0;
   }
 
   return count;
@@ -274,6 +350,7 @@ static ssize_t iob_pfsm_write(struct file *file, const char __user *buf,
  */
 static loff_t iob_pfsm_llseek(struct file *filp, loff_t offset, int whence) {
   loff_t new_pos = -1;
+  struct iob_data *iob_pfsm_data = (struct iob_data *)filp->private_data;
 
   switch (whence) {
   case SEEK_SET:
@@ -290,7 +367,7 @@ static loff_t iob_pfsm_llseek(struct file *filp, loff_t offset, int whence) {
   }
 
   // Check for valid bounds
-  if (new_pos < 0 || new_pos > iob_pfsm_data.regsize) {
+  if (new_pos < 0 || new_pos > iob_pfsm_data->regsize) {
     return -EINVAL;
   }
 
